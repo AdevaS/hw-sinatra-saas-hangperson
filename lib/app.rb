c068cb99b42edc439cb726b5f3f260de89f7d732
AdevaS <adevanir@gmail.com>
2015-11-02 22:19:36 -0200

    Ready for Heroku!

--- lib/app.rb
+++ lib/app.rb
@@ -6,31 +6,32 @@ class HangpersonApp < Sinatra::Base
 
   enable :sessions
   register Sinatra::Flash
-  
+
   before do
     # code that is called before EVERY HTTP request
   end
-  
+
   after do
     # code that is called after EVERY HTTP request
   end
-  
+
   get '/' do
     redirect '/new'
   end
-  
+
   get '/new' do
     erb :new
   end
-  
+
   post '/create' do
     # Don't change next line: it's necessary for autograder to work properly.
     word = params[:word] || HangpersonGame.get_random_word # don't change this line!
     # Don't change the above line: it's necessary for autograder to work properly.
-    # Your additional code goes here:
 
+    # Your additional code goes here:
+    
   end
-  
+
   post '/guess' do
     # get the guessed letter from params[:guess] (note: if user left it blank,
     #   params[:guess] will be nil)
@@ -41,7 +42,7 @@ class HangpersonApp < Sinatra::Base
     # Either way, the user should then be shown the main game screen ('show' action).
 
   end
-  
+
   get '/show' do
     # To show the game status, use the check_win_or_lose function.
     # If player wins (word completed), do the 'win' action instead.
@@ -49,15 +50,15 @@ class HangpersonApp < Sinatra::Base
     # Otherwise, show the contents of the 'show.erb' (main game view) template.
 
   end
-  
+
   get '/win' do
     # Player wins. WARNING: prevent cheating by making sure the game has really been won!
     #  If player tries to cheat, they should be shown the main game view instead.  (And
     #  you can optionally supply a "No cheating!" messaage.)
     # If they really did win, show the 'win' view template.
-    
+
   end
-  
+
   get '/lose' do
     # Player loses. WARNING: make sure the game has really been lost!
     # If they really did lose, show the 'win' view template.
@@ -69,5 +70,5 @@ class HangpersonApp < Sinatra::Base
       redirect '/show'
     end
   end
-  
+
 end
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -4,13 +4,65 @@ class HangpersonGame
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
   
+
+  attr_accessor :word, :guesses, :wrong_guesses, :hidden_word, :temp_word, :status
+
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @hidden_word = '-' * word.size
+    @temp_word = word
+    @status = ''
   end
+
+  def guess(letter)
+    letter.downcase! if /([A-Z])/.match(letter)
+    if /[^A-Za-z0-9]/.match(letter) || letter.nil? || letter.empty?
+      raise ArgumentError.new("You must inform a letter")
+    elsif @guesses.include?(letter)
+      return false
+    else
+      if @word.include?(letter)
+        if @guesses.include?(letter)      # does not change guess list if the letter is repeated
+          return false               # and return false for @valid
+        else
+          @guesses += letter            # add the letter guessed to the guess list
+          word_with_guesses   # change the character '-' inside the variable hidden_word for the equivalent letter
+        end
+      else
+        if @wrong_guesses.include?(letter)
+          return false
+        else
+          @wrong_guesses += letter
+        end
+      end
+    end
+  end
+
+  def word_with_guesses
+    check_win_or_lose
+    @guesses.each_char do |i|
+      while @temp_word.include?(i) && @hidden_word != @word
+        char_index = @temp_word.index(i)
+        @hidden_word[char_index] = i
+        @temp_word[char_index] = '-'
+      end
+    end
+      return @hidden_word
+  end
+
+  def check_win_or_lose
+    if @guesses.size == @word.size
+      @status = :win
+    elsif @wrong_guesses.size >= 6
+      @status = :lose
+    else
+      @status = :play
+    end
+  end
+
 
   def self.get_random_word
     require 'uri'
@@ -18,5 +70,4 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
-
 end
